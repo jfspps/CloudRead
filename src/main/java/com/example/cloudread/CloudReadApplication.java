b36a10f429eec066d10ee29ca1a6dab5b40cdbc9
jfspps <jfsapps@gmail.com>
2021-04-05 15:37:01 +0100

    first commit

---
+++ src/main/java/com/example/cloudread/CloudReadApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloudread;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudReadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudReadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cloudread/CloudReadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudread;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudReadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
